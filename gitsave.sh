307606870c9920ecef99e99d083f106a83c890c8
Bishal Ghosh <bishal.ghosh@students.iiserpune.ac.in>
2022-07-13 22:34:32 +0530

    changed all the orientation

--- gitsave.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-git init
-git remote add origin git@github.com:bi-altgg/Master-thesis.git
-git status
-git add .
-git commit -m"updated code"
-git push -u origin master
--- gitting.sh
+++
@@ -1,6 +0,0 @@
-git init
-git remote add origin git@github.com:bi-altgg/Master-thesis.git
-git status
-git add .
-git commit -m "updated codes"
-git push -u origin main
