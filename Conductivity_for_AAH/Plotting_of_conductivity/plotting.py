a1f46c327b9740fd3b5f8dbdacb841df82f79bf5
Bishal Ghosh <bishal.ghosh@students.iiserpune.ac.in>
2022-07-20 12:05:15 +0530

    made changes to plot

--- Conductivity_for_AAH/Plotting_of_conductivity/plotting.py
+++ Conductivity_for_AAH/Plotting_of_conductivity/plotting.py
@@ -7,11 +7,25 @@ import math,cmath
 from scipy.sparse import diags
 from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
                                AutoMinorLocator)
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["font.serif"] = ["Times New Roman"]
+
+SMALL_SIZE = 25
+MEDIUM_SIZE = 25
+BIGGER_SIZE = 25
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)
 s= 15
-sgstrn = [0.0,0.1,0.5,1.0,2.0,5.0,10.0]
+sgstrn = [0.0,0.1,2.0,5.0]
 colormark =[['green',"."],['blue',"."],['red',"."],['tab:grey',"."],['tab:cyan',"."],['tab:pink',"."],['tab:olive',"."]]
 ax1 = plt.subplot(131)
-for i in range(7):
+for i in range(4):
     plot = np.loadtxt('(0.5)datafile_for'+ str(sgstrn[i]) + '.txt', dtype = 'float')
     keep_point = np.where(plot > 1.0E-18)
     plot=plot[keep_point]
@@ -26,11 +40,11 @@ for i in range(7):
     plt.legend()
     plt.xlim((-3.5,+3.5))
 ax2 = plt.subplot(132)
-for i in range(7):
-    plot = np.loadtxt('(1.0)datafile_for'+ str(sgstrn[i]) + '.txt', dtype = 'float')
+for i in range(4):
+    plot = np.loadtxt("/home/bishal/Master's thesis/Codes/Conductivity_for_AAH/Plotting_of_conductivity/(1.0)datafile_for"+ str(sgstrn[i]) + ".txt", dtype = "float")
     keep_point = np.where(plot > 1.0E-18)
     plot=plot[keep_point]
-    free_energy = np.loadtxt('(0.5)free_energ.txt',dtype = 'float')
+    free_energy = np.loadtxt("/home/bishal/Master's thesis/Codes/Conductivity_for_AAH/Plotting_of_conductivity/(1.0)free_energ.txt",dtype = 'float')
     free_energy=free_energy[keep_point]
     plt.scatter(free_energy, plot,s,c = colormark[i][0],marker = colormark[i][1],alpha = .8,label =  f'$\gamma = {sgstrn[i]}$')
     plt.title('($\lambda = 1.0$)')
@@ -39,11 +53,11 @@ for i in range(7):
     plt.grid(True)
     plt.xlim((-3.5,+3.5))
 ax3 = plt.subplot(133)
-for i in range(7):
-    plot = np.loadtxt('(1.2)datafile_for'+ str(sgstrn[i]) + '.txt', dtype = 'float')
+for i in range(4):
+    plot = np.loadtxt("/home/bishal/Master's thesis/Codes/Conductivity_for_AAH/Plotting_of_conductivity/(1.2)datafile_for"+ str(sgstrn[i]) + ".txt", dtype = "float")
     keep_point = np.where(plot > 1.0E-18)
     plot=plot[keep_point]
-    free_energy = np.loadtxt('(0.5)free_energ.txt',dtype = 'float')
+    free_energy = np.loadtxt("/home/bishal/Master's thesis/Codes/Conductivity_for_AAH/Plotting_of_conductivity/(1.2)free_energ.txt",dtype = 'float')
     free_energy=free_energy[keep_point]
     plt.scatter(free_energy, plot,s,c = colormark[i][0],marker = colormark[i][1],alpha = .8,label =  f'$\gamma = {sgstrn[i]}$')
     plt.title('($\lambda = 1.2$)')
@@ -51,4 +65,5 @@ for i in range(7):
     plt.yscale('log')
     plt.grid(True)
     plt.xlim((-3.5,+3.5))
+plt.savefig('conductivity.pdf')
 plt.show()
